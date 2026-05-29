89485b3db0032fc6180231b1a135e98116ecba57
romasl1m <romanmaksimenka730@gmail.com>
2026-05-29 21:22:33 +0200

    railway

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,26 @@
 #include "core_routes.h"
 #include "api.h"
 #include "login.h"
-
-int main(){
+#include <cstdlib>
+// #include <string>
+int main() {
     crow::SimpleApp app;
 
     core_routes(app);
     register_auth_routes(app);
     login_logout_routes(app);
-    dashboard(app);        
+    dashboard(app);
     api_routes(app);
 
     // app.port(8080).multithreaded().run();
-    app.port(8080).bindaddr("0.0.0.0").multithreaded().run();
-    
+    // app.port(8080).bindaddr("0.0.0.0").multithreaded().run();
+    int port = 8080;
+
+    if (const char *p = getenv("PORT")) {
+        port = std::stoi(p);
+    }
+
+    app.port(port).multithreaded().run();
+
     return 0;
 }
