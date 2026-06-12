d98ded2891e1be5baa125eed2a1da4b2f3ce55e2
romasl1m <romanmaksimenka730@gmail.com>
2026-06-12 18:10:08 +0200

    phone

---
+++ templates/skeleton.cpp
@@ -0,0 +1,25 @@
+/*
+Now playing:
+__SPOTIFY__
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+bool comp(int a, int b) {
+    return a < b;
+}
+#define raindrop ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define droptop(a, n) for (int i = (a); i < (n); i++)
+#define cookie(t) while (t--)
+#define hotbox(wec) sort(wec.begin(), wec.end(), comp);
+#define pb push_back
+#define ll long long
+
+int main() {
+    raindrop;
+    int t = 1;
+    cookie(t)
+    {
+    }
+}
